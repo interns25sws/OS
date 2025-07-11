6e9b8fabcdb3a1e3420c656627976fa9a8d2d8fe
gandhi-neel <gandhineel270@gmail.com>
2025-07-11 18:06:56 +0530

    Auto-commit

--- frontend/src/ShopProducts/shop.jsx
+++ frontend/src/ShopProducts/shop.jsx
@@ -1,12 +1,19 @@
 import React from "react";
-import "./shop.css"
+import DisplayImage from "../assets/shopimages/maindisplay.jpg";
+import "./shop.css";
 
 export default function Shop() {
   return (
-    <section className="shop-section">
-      <div className="shop-container">
-        <h1 className="shop-heading">Welcome to Our Online Shop</h1>
-      </div>
-    </section>
+    <div className="shop-section">
+      <section className="shop-container">
+        <img src={DisplayImage} alt="Main Display" className="shop-image" />
+        <h1 className="shop-heading">Style yourself with proper fit</h1>
+      </section>
+
+      <section>
+        <h2 className="shop-subheading">Our Products</h2>
+        
+      </section>
+    </div>
   );
 }
