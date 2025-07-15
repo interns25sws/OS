8ecdf2a84b96e5cb0cde741e605394e612ddba92
gandhi-neel <gandhineel270@gmail.com>
2025-07-15 17:14:39 +0530

    Auto-commit

--- backend/seed.js
+++ backend/seed.js
@@ -1,14 +1,13 @@
-const mongoose = require('mongoose');
-const Product = require('./models/productModel');
-require('dotenv').config();
+const mongoose = require("mongoose");
+const Product = require("./models/productModel");
+require("dotenv").config();
 
 async function seedProducts() {
   try {
-    console.log('Connecting to DB...');
+    console.log("Connecting to DB...");
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to DB.');
+    console.log("Connected to DB.");
 
-    // Optional: Clear old products to avoid duplicates
     await Product.deleteMany();
 
     const products = [
@@ -17,32 +16,17 @@ async function seedProducts() {
         price: 20,
         description: "Cool cotton t-shirt",
         sizes: ["S", "M", "L"],
-        images: ["http://localhost:5000/images/Newhoodie.jpg"]
+        images: ["http://localhost:5000/images/Newhoodie.jpg"],
       },
-      {
-        name: "Jeans",
-        price: 40,
-        description: "Stylish denim jeans",
-        sizes: ["M", "L", "XL"],
-        images: ["http://localhost:5000/images/Allbottom.jpg"]
-      },
-
-      {
-        name: "Sneakers",
-        price: 60,
-        description: "Comfortable sneakers",
-        sizes: ["M", "L", "XL"],
-        images: ["http://localhost:5000/images/sneak  ers.jpg"]
-      }
     ];
 
     await Product.insertMany(products);
     console.log("Products added!");
   } catch (err) {
-    console.error('Error seeding products:', err);
+    console.error("Error seeding products:", err);
   } finally {
     await mongoose.disconnect();
-    console.log('Disconnected from DB.');
+    console.log("Disconnected from DB.");
   }
 }
 
